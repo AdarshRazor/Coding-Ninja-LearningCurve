2eba12f4ac525ec7449aec07eb0b5bc2d5f78e06
Adarsh Anand <adarshanshu7@gmail.com>
2024-06-21 21:40:26 +0530

    updated

--- React/react_hooks/src/App.js
+++ React/react_hooks/src/App.js
@@ -3,16 +3,20 @@ import StateEventhandle from './components/state_eventhandle';
 import StateEventhandleClass from './components/state_eventhandleclass';
 import DarkMode from './components/useState/DarkMode';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
+    <Router>
     <Navbar/>
     <div className="container my-5">
-      {/* <StateEventhandle heading="Text Analyzer Function" /> */}
-      {/* <StateEventhandleClass heading="Text Analyzer class" /> */}
-      <DarkMode/>
+      <Routes>
+        <Route exact path="/text" element={<StateEventhandle heading="Text Analyzer Function" />} />
+        <Route path="/" element={<DarkMode/>} />
+      </Routes>
     </div>
+    </Router>
   </>
   );
 }
--- React/react_hooks/src/components/Navbar.js
+++ React/react_hooks/src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './Navbar.css'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
   return (
     <nav className='navbar'>
         <div className='navbar-brand'>NavBar</div>
         <ul className='navbar-links'>
-            <li className='navbar-items'><a href='#home'>Home</a></li>
-            <li className='navbar-items'><a href='#home'>Home</a></li>
-            <li className='navbar-items'><a href='#home'>Home</a></li>
-            <li className='navbar-items'><a href='#home'>Home</a></li>
+            {/* <li className='navbar-items'><a href='#home'>Home</a></li> */}
+            <li className='navbar-items'> <Link to="/text">Text</Link> </li>
+            <li className='navbar-items'> <Link to="/">About</Link> </li>
         </ul>
     </nav>
   )
