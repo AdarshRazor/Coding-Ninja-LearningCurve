c345b0cf38c68bf6052bd3f0ad396923c9a13142
Adarsh Anand <adarshanshu7@gmail.com>
2024-07-10 00:21:59 +0530

    updated

--- MERN/Notebook/server/routes/auth.js
+++ MERN/Notebook/server/routes/auth.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const fetchuser = require("../middleware/fetchuser");
 
-const JWT_SECRET = 'Random@TokenSecret12324567899'
+const JWT_SECRET = "Random@TokenSecret12324567899";
 
-// Create a user using: POST "/api/auth/createuser". No login required
+// ROUTE 1 : Create a user using: POST "/api/auth/createuser". No login required
 router.post(
   "/createuser",
   [
@@ -25,8 +26,6 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    
-
     console.log(req.body);
 
     try {
@@ -38,8 +37,8 @@ router.post(
       }
 
       // encrpt the password
-      const salt= await bcrypt.genSalt(10);
-      const secPass = await bcrypt.hash(req.body.password, salt)
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
 
       user = new User({
         name: req.body.name,
@@ -51,8 +50,8 @@ router.post(
 
       // res.status(201).json(user);
 
-      const data = { user: { id: user.id}};
-      const authToken = jwt.sign(data, JWT_SECRET)
+      const data = { user: { id: user.id } };
+      const authToken = jwt.sign(data, JWT_SECRET);
 
       res.json({
         token: authToken,
@@ -61,11 +60,79 @@ router.post(
         //   name: user.name,
         //   email: user.email
         // }
-      })
-
+      });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: error.message });
+    }
+  }
+);
+
+// ROUTE 2 : Login a user using: POST "/api/auth/login". No login required
+
+router.post(
+  "/login",
+  [
+    // middlware to valid email.
+    body("email").isEmail().withMessage("Please enter a valid email"),
+    body("password").notEmpty().withMessage("Password is required"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // destructure
+    const { email, password } = req.body;
+    try {
+      // if email does not exist.
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({
+            errors: [
+              {
+                msg: "Invalid Credentials, Please try to login with correct credentials",
+              },
+            ],
+          });
+      }
+      // compare password
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({
+            errors: [
+              {
+                msg: "Invalid Credentials, Please try to login with correct credentials",
+              },
+            ],
+          });
+      }
+
+      const data = { user: { id: user.id } };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken: authToken });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: error.message });
+    }
+  }
+);
+
+// ROUTE 3 : Get logged in user details "/api/auth/getuser". Login required
+router.post(
+  "/getuser", fetchuser, 
+  async (req, res) => {
+    try {
+      userId = req.user.id;
+      const user = await User.findById(userId).select("-password");
+      res.send(user)
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "error.message" });
     }
   }
 );
