e2aac4947f9bdd7fd0fe3797d785d9b12a73ebe2
Adarsh Anand <adarshanshu7@gmail.com>
2024-07-23 20:59:03 +0530

    updated

--- React/react-redux/src/state/index.js
+++
@@ -1,2 +0,0 @@
-// all the action creators will be in this folder
-export * as actionCCreators from './actionCreators/index';
--- React/react-redux/src/state/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import amountRReducer from './amountReducer';
-
-const rootreducers = combineReducers({
-    amount: amountRReducer
-    // other reducers can be added here
-})
-
-export default rootreducers;
